ab9896e6ab2feab1d80c20705f09459e1f191359
lucashomero <lucashomerovs@gmail.com>
2025-05-22 15:37:31 -0300

    feat: form page #26

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MomentFormComponent } from '../../moment-form/moment-form.component';
+import { NewMomentComponent } from '../new-moment/new-moment.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MomentFormComponent, NewMomentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
