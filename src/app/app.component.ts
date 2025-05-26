71c2ff22fb48f6b91ce51ad06ed2c78e5fc1b5ab
lucashomero <lucashomerovs@gmail.com>
2025-05-26 16:27:28 -0300

    message component structure #30

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MessagesComponent } from './components/messages/messages.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NewMomentComponent, FormsModule, ReactiveFormsModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NewMomentComponent, FormsModule, ReactiveFormsModule, MessagesComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
