c7bcf250cc06e10878e78b700b83642d1c94cfb7
lucashomero <lucashomerovs@gmail.com>
2025-05-27 14:54:53 -0300

    feat: fetch data from database #32

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -2,14 +2,41 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MomentFormComponent } from '../../moment-form/moment-form.component';
 import { NewMomentComponent } from '../new-moment/new-moment.component';
+import { Moment } from '../../../Moment';
+import { environment } from '../../../../environments/environment';
+import { MomentService } from '../../../services/moment.service';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, MomentFormComponent, NewMomentComponent],
+  imports: [RouterOutlet, MomentFormComponent, NewMomentComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  allMoments: Moment[] = []
+  moments: Moment[] = []
+  baseApiUrl = environment.baseApiUrl
+
+  // to do search
+
+  constructor(private momentService: MomentService){}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data
+      this.moments = data
+    });
+  }
 
 }
